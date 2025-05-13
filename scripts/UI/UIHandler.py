839dad4266d41eac3d39c5e7c1f1fa5e015421a6
daprince1110 <daprince1110@users.noreply.github.com>
2025-05-13 03:10:27 -0500

    WIP: local changes before pulling

--- scripts/UI/UIHandler.py
+++ scripts/UI/UIHandler.py
@@ -3,9 +3,12 @@ from scripts.UI.main_menu import MainMenu
 from scripts.UI.char_select import Character
 from scripts.UI.difficulty import DifficultySelector
 
+
+
 class UIHandler:
     def __init__(self, screen):
         self.screen = screen
+        
        # self.Shop = Shop()
        # self.Character = Character()
     def render(self,atr):
@@ -23,6 +26,7 @@ class UIHandler:
             case 'difficulty':
                 DifficultySelector.initialize(self.screen)
                 DifficultySelector.draw(self.screen)
+        
     def stop(self, atr):
         match atr:
             case'menu':
@@ -32,3 +36,5 @@ class UIHandler:
             case _:
                 MainMenu.stop()
                 Character.stop()
+    def setAction(self):
+        pass
